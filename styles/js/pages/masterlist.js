da0c5a5e126b6dd27d30485e8cb8ad0f49f2b5cb
vesselsarpg <vesselsarpg@gmail.com>
2025-12-27 01:22:08 -0500

    Update masterlist.js

--- styles/js/pages/masterlist.js
+++ styles/js/pages/masterlist.js
@@ -16,7 +16,14 @@ document.addEventListener("DOMContentLoaded", async () => {
     async (listData) => {
 
       if (listData.type == 'profile') {
-
+        
+          // Create the image gallery
+  if (charadex.tools.checkArray(listData.profileArray[0].imagegallery)) {
+    let gallery = await charadex.initialize.page(
+      listData.profileArray[0].imagegallery,
+      charadex.page.masterlist.relatedData['image gallery']
+    );
+  }
         // Create the log dex
         if (charadex.tools.checkArray(listData.profileArray[0].masterlistlog)) {
           let logs = await charadex.initialize.page(
